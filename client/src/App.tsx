93a53300b05198d37c1bf748171d6af1b99a1767
ritikprajapat21 <ritikpfb21@gmail.com>
2023-12-10 09:55:18 +0530

    Connected Backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,92 @@
-import React, { useState } from 'react';
-import { DragDropContext } from 'react-beautiful-dnd';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
+import "./App.css";
 
-import InputField from './components/InputField';
-import { Todo } from './components/model';
-import TodoList from './components/TodoList';
+import InputField from "./components/InputField";
+import { Todo } from "./components/model";
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
+  // For user Input
+  const [todo, setTodo] = useState<string>("");
 
-  const [todo, setTodo] = useState<string>('')
+  // For list of todos
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const [todos, setTodos] = useState<Todo[]>([])
+  // For todos set completed
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
 
-  const [completedTodos, setCompletedTodos] = useState<Todo[]>([])
+  useEffect(() => {
+    const getTodos = async () => {
+      try {
+        // Fetching created todos from database
+        const response = await fetch("http://localhost:5000/todos");
+        const data = await response.json();
 
-  const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault()
+        console.log(data);
+        setTodos((prev) => (prev ? [...prev, ...data] : [...data]));
+      } catch (error: any) {
+        console.error(error.message);
+      }
+    };
+
+    getTodos();
+  }, []);
+
+  const handleAdd = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const newTodo = { id: todos.length + 1, todo, isDone: false };
     if (todo) {
-      setTodos([...todos, { id: Date.now(), todo, isDone: false }])
-      setTodo('')
+      setTodos([...todos, newTodo]);
+      setTodo("");
     }
-  }
+
+    const response = await fetch("http://localhost:5000/todos", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newTodo),
+    });
+
+    console.log(response);
+  };
+
+  const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result;
+
+    if (!destination) return;
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+
+    let add,
+      active = [...todos],
+      complete = [...completedTodos];
+
+    if (source.droppableId === "TodoList") {
+      add = active.splice(source.index, 1)[0];
+    } else {
+      add = complete.splice(source.index, 1)[0];
+    }
+
+    if (destination.droppableId === "CompletedList") {
+      add = { ...add, isDone: true };
+      complete.splice(destination.index, 0, add);
+    } else {
+      add = { ...add, isDone: false };
+      active.splice(destination.index, 0, add);
+    }
+
+    setTodos(active);
+    setCompletedTodos(complete);
+  };
 
   return (
-    <DragDropContext onDragEnd={() => {}}>
-      <div className='App'>
-        <span className="heading">Taskify</span>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="App">
+        <span className="heading">Task Manager</span>
         <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
         <TodoList
           todos={todos}
@@ -36,6 +97,6 @@ const App: React.FC = () => {
       </div>
     </DragDropContext>
   );
-}
+};
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,90 @@
-const express = require("express")
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+
+const pool = require("./db");
 
 //middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+
+app.use(morgan("tiny"));
+
+// Routes
+
+// Create a todo
+
+app.post("/todos", async (req, res) => {
+  try {
+    const { todo } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (todo) VALUES ($1) RETURNING *",
+      [todo]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// Get all todo
+
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+
+    res.json(allTodos.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// Get a todo
+
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const todo = await pool.query("SELECT * FROM todo  WHERE id = $1", [id]);
+
+    res.json(todo.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// Update a todo
+
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { todo } = req.body;
+
+    const updateTodo = await pool.query(
+      "UPDATE todo SET todo = $1 WHERE id = $2",
+      [id, todo]
+    );
+
+    res.json(updateTodo.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// Delete a todo
+
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE id = $1", [id]);
+
+    res.json("Todi was deleted");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 app.listen(5000, () => {
-    console.log("Server is running on port 5000")
-})
+  console.log("Server is running on port 5000");
+});
